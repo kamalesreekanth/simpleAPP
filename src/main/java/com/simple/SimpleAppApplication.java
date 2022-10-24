eacdb58336149541f8f3579e4bcd879430f3d087
kamale sreekanth <MY_NAME@example.com>
2022-10-24 18:54:08 +0530

    files4

---
+++ src/main/java/com/simple/SimpleAppApplication.java
@@ -0,0 +1,21 @@
+package com.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SimpleAppApplication {
+	
+	@GetMapping("/home")
+	public String welcome() {
+		return "welcome";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAppApplication.class, args);
+	}
+
+}
